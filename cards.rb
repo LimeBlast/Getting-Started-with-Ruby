ff95b2c2f618d7bf6988bb3abaf3cdc1eb1c4740
Daniel Hollands <daniel@limeblast.co.uk>
2014-04-25 19:16:31 +0100

    added a card generator

---
+++ cards.rb
@@ -0,0 +1,12 @@
+deck = []
+count = 0
+suits = %w{Hearts Diamonds Clubs Spades}
+values = %w{Ace 2 3 4 5 6 7 8 9 10 Jack Queen King}
+suits.each do |suit|
+  values.each do |value|
+    deck << "#{value} of #{suit}"
+    count = count.next # increment the count
+  end
+end
+puts deck.join(', ')
+puts "There are #{count} cards"
