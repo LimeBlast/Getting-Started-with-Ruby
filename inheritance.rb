3c99a4ecb76b87c9c9d101dc8b87359692738850
Daniel Hollands <daniel@limeblast.co.uk>
2014-04-26 11:23:16 +0100

    Added some comments

--- inheritance.rb
+++ inheritance.rb
@@ -1,19 +1,24 @@
 require './animal.rb'
 
+# extending the Animal class
 class Rabbit < Animal
 
+  # creating a new method
   def hop
     'Hopping is great fun'
   end
 
 end
 
+# extend the Animal class
 class Duck < Animal
 
+  # creating a new method
   def swim
     "I'm quackers about swimming!"
   end
 
+  # overwriting an existing method
   def say_hello
     "Quack! I'm a duck called #{ @name }"
   end
